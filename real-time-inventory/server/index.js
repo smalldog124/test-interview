d2418a07fb9f9ce8271db2261e926d802d7c85a7
smalldog124 <panumars124@gmail.com>
2025-04-14 21:18:26 +0700

    move handlers to /product

--- real-time-inventory/server/index.js
+++ real-time-inventory/server/index.js
@@ -1,11 +1,21 @@
-import pgPool from "./db-connection/pg.js";
-import { getAllProduct, reduceStock, getProductById } from "./product/index.js";
+import ProductHandler from "./product/handlers.js";
 import express, { json, urlencoded } from "express";
 import { WebSocketServer } from "ws";
 import rateLimit from "express-rate-limit";
 import path from "path";
 import { fileURLToPath } from "url";
+import pgPool from "./db-connection/pg.js";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const wss = new WebSocketServer({ port: 30001 });
+wss.on("connection", (ws) => {
+  console.log("WebSocket connection established");
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+  });
+});
+console.log(`WebSocket server running at ws://localhost:30001`);
+const productHandler = new ProductHandler(wss, pgPool);
 const app = express();
 const port = 3000;
 app.use(json());
@@ -17,55 +27,11 @@ const apiLimiter = rateLimit({
 });
 
 app.use("/api/", apiLimiter);
-const wss = new WebSocketServer({ port: 30001 });
-wss.on("connection", (ws) => {
-  console.log("WebSocket connection established");
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-  });
+app.get("/api/v1/product", (req, res) => {
+  productHandler.getProducts(req, res);
 });
-
-app.get("/api/v1/product", async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const pageSize = parseInt(req.query.pageSize) || 10;
-  try {
-    const products = await getAllProduct(pgPool, page, pageSize);
-    if (!products) {
-      return res.status(404).json({ error: "Products not found" });
-    }
-    res.status(200).json(products);
-  } catch (err) {
-    console.error("❌ Error occurred:", err.message);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-app.post("/api/v1/product/reduce-stock", async (req, res) => {
-  console.log("req.body", req.body);
-  const { productId, quantity } = req.body;
-  if (!productId || !quantity) {
-    return res
-      .status(400)
-      .json({ error: "Product ID and quantity are required" });
-  }
-  try {
-    await reduceStock(pgPool, productId, quantity);
-    const product = await getProductById(pgPool, productId);
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
-    } else {
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(
-            JSON.stringify({ data: product, action: "update_stock" })
-          );
-        }
-      });
-    }
-    res.status(200).json({ message: "Stock reduced successfully" });
-  } catch (err) {
-    console.error("❌ Error occurred:", err.message);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
+app.post("/api/v1/product/reduce-stock", (req, res) => {
+  productHandler.reduceStock(req, res);
 });
 app.use("/static", express.static(path.join(__dirname, "../web")));
 app.listen(port, () => {
---
+++ real-time-inventory/server/product/handlers.js
@@ -0,0 +1,66 @@
+import { getAllProduct, reduceStock, getProductById } from "./repo.js";
+import WebSocket from 'ws';
+class ProductHandler {
+  constructor(wss, db) {
+      this.WebSocketServer = wss;
+      this.DBConnection = db;
+  }
+
+  async getProducts(req, res) {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const pageSize = parseInt(req.query.pageSize) || 10;
+      const products = await getAllProduct(this.DBConnection, page, pageSize);
+      res.status(200).json(products);
+    } catch (error) {
+      console.log("❌ Error occurred:", error.message);
+      res.status(500).json({ error: "Failed to fetch products" });
+    }
+  }
+
+  async getProductById(req, res) {
+    try {
+      const productId = req.params.id;
+      const product = await getProductById(this.DBConnection, productId);
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+      res.status(200).json(product);
+    } catch (error) {
+      console.log("❌ Error occurred:", error.message);
+      res.status(500).json({ error: "Failed to fetch product" });
+    }
+  }
+  async reduceStock(req, res) {
+    try {
+      const { productId, quantity } = req.body;
+      if (!productId || !quantity) {
+        return res
+          .status(400)
+          .json({ error: "Product ID and quantity are required" });
+      }
+      const success = await reduceStock(this.DBConnection, productId, quantity);
+      if (!success) {
+        return res
+          .status(404)
+          .json({ error: "Product not found or insufficient stock" });
+      }
+      const product = await getProductById(this.DBConnection, productId);
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+      this.WebSocketServer.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({ data: product, action: "update_stock" })
+          );
+        }
+      });
+      res.status(200).json({ message: "Stock reduced successfully" });
+    } catch (error) {
+      console.log("❌ Error occurred:", error.message);
+      res.status(500).json({ error: "Failed to reduce stock" });
+    }
+  }
+}
+export default ProductHandler;
